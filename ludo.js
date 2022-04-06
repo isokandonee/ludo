ef34140f767602bed122d5e40c04936cb49b39bf
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2022-04-06 22:56:00 +0100

    A little more finessing

--- ludo.js
+++ ludo.js
@@ -1,7 +1,21 @@
-// function roll 2 dice
+// function to roll dice
 function rollDice() {
     var dice1 = Math.floor(Math.random() * 6) + 1;
     var dice2 = Math.floor(Math.random() * 6) + 1;
     var diceTotal = dice1 + dice2;
     return diceTotal;
+}
+
+// miltiplayer function
+function multiPlayer() {
+    
+}
+
+// function to check if player has won
+function checkWin(diceTotal) {
+    if (diceTotal === 7) {
+        return true;
+    } else {
+        return false;
+    }
 }
